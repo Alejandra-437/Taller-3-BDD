c47de78a3183ef98340f961302d9b8628fc95f86
Alejandra-437 <00100520@uca.edu.sv>
2022-06-05 16:05:14 -0600

    chale

--- [Taller III] Consultas SQL - Grupo 10.sql
+++ [Taller III] Consultas SQL - Grupo 10.sql
@@ -6,7 +6,7 @@ USE TALLER_03_DATABANK;
 
 /*  1. Mostrar la lista de clientes que han contratado el plan "Premium". Ordenar el resultado con respecto al id del cliente en orden ascendente. Almacenar el resultado en una nueva tabla llamada "CLIENTES_PREMIUM”. */
 
-
+Select * from tipo_plan;
 
  ---- CONSULTA 2 ----
 
