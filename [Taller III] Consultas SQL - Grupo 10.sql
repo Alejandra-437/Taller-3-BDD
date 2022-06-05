99b797139c37b6fe463be17a8d24c8ca3689d283
HenryLima07 <00033721@uca.edu.sv>
2022-06-05 17:43:19 -0600

    Add: new feature

--- [Taller III] Consultas SQL - Grupo 10.sql
+++ [Taller III] Consultas SQL - Grupo 10.sql
@@ -4,41 +4,56 @@ USE TALLER_03_DATABANK;
 
  ---- CONSULTA 1 ----
 
-/*  1. Mostrar la lista de clientes que han contratado el plan "Premium". Ordenar el resultado con respecto al id del cliente en orden ascendente. Almacenar el resultado en una nueva tabla llamada "CLIENTES_PREMIUM”. */
+/*  1. Mostrar la lista de clientes que han contratado el plan "Premium". Ordenar el resultado con respecto al id del cliente en orden ascendente. Almacenar el resultado en una nueva tabla llamada "CLIENTES_PREMIUMï¿½. */
 
+SELECT C.id, C.nombre, C.direccion 
+INTO CLIENTES_PREMIUM
+FROM CLIENTE C
+INNER JOIN TIPO_PLAN TP
+ON C.id_tipo_plan = TP.id
+WHERE TP.tipo = 'Premium'
+ORDER BY C.id ASC;
 
+--SELECT * FROM CLIENTES_PREMIUM;
 
  ---- CONSULTA 2 ----
 
-/* 2. Mostrar las 2 clínicas más populares. El parámetro de popularidad se define en base al número de citas registradas por cada clínica. Mostrar el id de la clínica, el nombre, su dirección y email, además mostrar la cantidad de citas registradas. Ordenar el resultado en base a la cantidad de citas registradas. */
+/* 2. Mostrar las 2 clï¿½nicas mï¿½s populares. El parï¿½metro de popularidad se define en base al nï¿½mero de citas registradas por cada clï¿½nica. Mostrar el id de la clï¿½nica, el nombre, su direcciï¿½n y email, ademï¿½s mostrar la cantidad de citas registradas. Ordenar el resultado en base a la cantidad de citas registradas. */
 
 
 
  ---- CONSULTA 3 ----
 
-/* 3. Mostrar la información completa de cada cliente, incluir el nombre, dirección, el tipo de plan, los correos (si es que ha brindado alguno) y los teléfonos (si es que ha brindado alguno). Ordenar el resultado con respecto al id del cliente en orden ascendente.  */
+/* 3. Mostrar la informaciï¿½n completa de cada cliente, incluir el nombre, direcciï¿½n, el tipo de plan, los correos (si es que ha brindado alguno) y los telï¿½fonos (si es que ha brindado alguno). Ordenar el resultado con respecto al id del cliente en orden ascendente.  */
 
 
 ---- CONSULTA 4 ----
 
-/* 4. Identificar las consultas que han necesitado de un médico asistente, mostrar el id de la consulta, la fecha, la duración, el id del médico y el nombre del médico asistente. Ordenar el resultado con respecto al id de la consulta en orden ascendente.  */
+/* 4. Identificar las consultas que han necesitado de un mï¿½dico asistente, mostrar el id de la consulta, la fecha, la duraciï¿½n, el id del mï¿½dico y el nombre del mï¿½dico asistente. Ordenar el resultado con respecto al id de la consulta en orden ascendente.  */
 
+SELECT C.id 'ID Consulta', C.fecha, C.duracion, M.id 'ID MÃ©dico', M.nombre FROM CONSULTA C
+INNER JOIN MEDICOXCONSULTA MC
+ON C.id = MC.id_consulta
+INNER JOIN MEDICO M
+ON MC.id_medico = M.id
+WHERE MC.rol = 0
+ORDER BY C.id ASC;
 
 ---- CONSULTA 5 ----
 
 
-/* 5. ¿Cuáles son las clínicas capacitadas para atender emergencias? Mostrar el id de la clínica, el nombre, la dirección y email. */
+/* 5. ï¿½Cuï¿½les son las clï¿½nicas capacitadas para atender emergencias? Mostrar el id de la clï¿½nica, el nombre, la direcciï¿½n y email. */
 
 
 ---- CONSULTA 6 ----
 
-/* 6. Calcular las ganancias de la asociación en la primera quincena de mayo. Mostrar la fecha de la consulta, el nombre del cliente atendido y el nombre del médico principal. Se debe considerar que existe la posibilidad de que haya consultas en las que no se recete ningún medicamento. Ordenar el resultado con respecto al id de la consulta en orden ascendente. Las ganancias de cada consulta se calculan de la siguiente forma: (Precio de la consulta + Suma de todos los medicamentos recetados) + 13% IVA.  */
+/* 6. Calcular las ganancias de la asociaciï¿½n en la primera quincena de mayo. Mostrar la fecha de la consulta, el nombre del cliente atendido y el nombre del mï¿½dico principal. Se debe considerar que existe la posibilidad de que haya consultas en las que no se recete ningï¿½n medicamento. Ordenar el resultado con respecto al id de la consulta en orden ascendente. Las ganancias de cada consulta se calculan de la siguiente forma: (Precio de la consulta + Suma de todos los medicamentos recetados) + 13% IVA.  */
 
 
 
 ---- CONSULTA 7 ----
 
-/* 7. El comité de dirección planea realizar una fuerte inversión con el objetivo de establecer a la asociación como el consorcio líder a nivel nacional, para verificar la viabilidad del proyecto, el comité ha solicitado un reporte especial que consiste en mostrar las ganancias del mes de mayo de 2022 pero organizadas en base a 4 grupos de fechas. Por acuerdo del comité, los 4 grupos son los siguientes: 
+/* 7. El comitï¿½ de direcciï¿½n planea realizar una fuerte inversiï¿½n con el objetivo de establecer a la asociaciï¿½n como el consorcio lï¿½der a nivel nacional, para verificar la viabilidad del proyecto, el comitï¿½ ha solicitado un reporte especial que consiste en mostrar las ganancias del mes de mayo de 2022 pero organizadas en base a 4 grupos de fechas. Por acuerdo del comitï¿½, los 4 grupos son los siguientes: 
                 -- Semana 1:    Del 1 al 8 de mayo.     
                 -- Semana 2:    Del 8 al 15 de mayo.        tal vez deberia ser del 9 al 15
                 -- Semana 3:    Del 16 al 22 de mayo.       
